8f928e93ad4d15abf21b8ff95625dc8e3867e5d0
Elias de Francisco <eliasdefrancisco@gmail.com>
2021-08-07 21:18:37 +0200

    Tests for Mongo service and Change Streams

---
+++ src/integrationTests/mongoChangeStreams.test.ts
@@ -0,0 +1,295 @@
+import { ChangeStream, Collection, MongoClient } from "mongodb"
+import config from "../config"
+import changeStreamOperations from "../enums/changeStreamOperations"
+
+
+describe('Change Streams integration tests from Mongo Daemon', () => {
+    let changeStream: ChangeStream<Document>
+    let collection: Collection
+    let mongoClient: MongoClient
+
+    beforeAll(async () => {
+        await MongoClient.connect(config.mongoUri)
+            .then(client => {
+                mongoClient = client
+                const db = mongoClient.db(config.dbNameTest)
+                collection = db.collection(config.collectionOnFire)
+                // changeStream = collection.watch()
+                collection.deleteMany({})
+            })
+    })
+
+    afterAll(() => {
+        changeStream.close()
+        mongoClient.close()
+    })
+
+    test('Insert must return some needed fields', done => {
+        changeStream = collection.watch()
+        changeStream.on('change', change => {
+            try {
+                const operation = change.operationType
+                const documentKey: any = change.documentKey
+                const fullDocument: any = change.fullDocument
+                const ns = change.ns
+                expect(ns).toEqual({ db: config.dbNameTest, coll: config.collectionOnFire })
+                expect(operation).toEqual(changeStreamOperations.insert)
+                expect(documentKey._id).toBeDefined()
+                expect(fullDocument._id).toBeDefined()
+                expect(fullDocument.num1).toEqual(1)
+                expect(fullDocument.num2).toEqual(2)
+                expect(fullDocument.str1).toEqual('dummy1')
+                expect(fullDocument.str2).toEqual('dummy2')
+                expect(fullDocument.arr).toEqual([1, 2, 3, 4, 5])
+                expect(fullDocument.obj).toEqual({ aa: 11, bb: 'dummyNested', cc: [11, 22, 33, 44, 55] })
+                changeStream.close()
+                done()
+            }
+            catch (err) {
+                done(err)
+            }
+        })
+        const batch = async () => {
+            await collection.insertOne({
+                num1: 1,
+                num2: 2,
+                str1: 'dummy1',
+                str2: 'dummy2',
+                arr: [1, 2, 3, 4, 5],
+                obj: { aa: 11, bb: 'dummyNested', cc: [11, 22, 33, 44, 55] }
+            })
+        }
+        setImmediate(batch)
+    })
+
+    test('Delete must return some needed fields', done => {
+        let insertedId: any
+        changeStream = collection.watch()
+        changeStream.on('change', change => {
+            try {
+                const documentKey: any = change.documentKey
+                const ns = change.ns
+                const operation: string = change.operationType
+                if (operation === changeStreamOperations.insert) {
+                    insertedId = documentKey._id
+                }
+                else if (operation === changeStreamOperations.delete) {
+                    expect(ns).toEqual({ db: config.dbNameTest, coll: config.collectionOnFire })
+                    expect(documentKey._id).toEqual(insertedId)
+                    changeStream.close()
+                    done()
+                }
+            }
+            catch (err) {
+                done(err)
+            }
+        })
+        const batch = async () => {
+            await collection.insertOne({ aa: 11, bb: 22 })
+            await collection.deleteOne({ aa: 11, bb: 22 })
+        }
+        setImmediate(batch)
+    })
+
+    test('Replace must return some needed fields', done => {
+        let insertedId: any
+        changeStream = collection.watch()
+        changeStream.on('change', change => {
+            try {
+                const documentKey: any = change.documentKey
+                const ns = change.ns
+                const fullDocument: any = change.fullDocument
+                const operation: string = change.operationType
+
+                if (operation === changeStreamOperations.insert) {
+                    insertedId = documentKey._id
+                }
+                else if (operation === changeStreamOperations.replace) {
+                    expect(ns).toEqual({ db: config.dbNameTest, coll: config.collectionOnFire })
+                    expect(documentKey._id).toEqual(insertedId)
+                    expect(fullDocument._id).toBeDefined()
+                    expect(fullDocument).toEqual({ _id: insertedId, aa: 1111, bb: 2222, cc: 3333 })
+                    changeStream.close()
+                    done()
+                }
+            }
+            catch (err) {
+                done(err)
+            }
+        })
+        const batch = async () => {
+            await collection.insertOne({ aa: 111, bb: 222 })
+            await collection.updateOne({ aa: 111 }, [{ $set: { aa: 1111, bb: 2222, cc: 3333 } }])
+        }
+        setImmediate(batch)
+    })
+
+    test('Update must return some needed fields when modify document from collection with $set', done => {
+        changeStream = collection.watch()
+        changeStream.on('change', change => {
+            try {
+                const operation = change.operationType
+                const documentKey: any = change.documentKey
+                const updatedFields = change.updateDescription?.updatedFields
+                const ns = change.ns
+                expect(ns).toEqual({ db: config.dbNameTest, coll: config.collectionOnFire })
+                expect(operation).toEqual(changeStreamOperations.update)
+                expect(documentKey._id).toBeDefined()
+                expect(updatedFields).toEqual({ str2: 'updatedStr2', num2: 22, num3: 33 })
+                changeStream.close()
+                done()
+            }
+            catch (err) {
+                done(err)
+            }
+        })
+        const batch = async () => {
+            await collection.updateOne(
+                { num1: 1 },
+                { $set: { str2: 'updatedStr2', num2: 22, num3: 33 } }
+            )
+        }
+        setImmediate(batch)
+    })
+
+    test('Update must return some needed fields when modify array field from document with $pull', done => {
+        changeStream = collection.watch()
+        changeStream.on('change', change => {
+
+            try {
+                const operation = change.operationType
+                const documentKey: any = change.documentKey
+                const updatedFields = change.updateDescription?.updatedFields
+                const ns = change.ns
+                expect(ns).toEqual({ db: config.dbNameTest, coll: config.collectionOnFire })
+                expect(operation).toEqual(changeStreamOperations.update)
+                expect(documentKey._id).toBeDefined()
+                expect(updatedFields).toEqual({ arr: [1, 2, 3, 5] })
+                changeStream.close()
+                done()
+            }
+            catch (err) {
+                done(err)
+            }
+        })
+        const batch = async () => {
+            await collection.updateOne(
+                { num1: 1 },
+                { $pull: { arr: 4 } }
+            )
+        }
+        setImmediate(batch)
+    })
+
+    test('Update must return some needed fields when modify array field from document with $push', done => {
+        changeStream = collection.watch()
+        changeStream.on('change', change => {
+            try {
+                const operation = change.operationType
+                const documentKey: any = change.documentKey
+                const updatedFields = change.updateDescription?.updatedFields
+                const ns = change.ns
+                expect(ns).toEqual({ db: config.dbNameTest, coll: config.collectionOnFire })
+                expect(operation).toEqual(changeStreamOperations.update)
+                expect(documentKey._id).toBeDefined()
+                expect(updatedFields).toEqual({ 'arr.4': 6, 'arr.5': 7, 'arr.6': 8 })
+                changeStream.close()
+                done()
+            }
+            catch (err) {
+                done(err)
+            }
+        })
+        const batch = async () => {
+            await collection.updateOne(
+                { num1: 1 },
+                { $push: { arr: { $each: [6, 7, 8] } } }
+            )
+        }
+        setImmediate(batch)
+    })
+
+    test('Update must return some needed fields when modify object nested field from document with $set', done => {
+        changeStream = collection.watch()
+        changeStream.on('change', change => {
+            try {
+                const operation = change.operationType
+                const documentKey: any = change.documentKey
+                const updatedFields = change.updateDescription?.updatedFields
+                const ns = change.ns
+                expect(ns).toEqual({ db: config.dbNameTest, coll: config.collectionOnFire })
+                expect(operation).toEqual(changeStreamOperations.update)
+                expect(documentKey._id).toBeDefined()
+                expect(updatedFields).toEqual({ 'obj.addedNum': 666 })
+                changeStream.close()
+                done()
+            }
+            catch (err) {
+                done(err)
+            }
+        })
+        const batch = async () => {
+            await collection.updateOne(
+                { num1: 1 },
+                { $set: { 'obj.addedNum': 666 } }
+            )
+        }
+        setImmediate(batch)
+    })
+
+    test('Update must return some needed fields when remove field from document with $unset', done => {
+        changeStream = collection.watch()
+        changeStream.on('change', change => {
+            try {
+                const operation = change.operationType
+                const documentKey: any = change.documentKey
+                const removedFields = change.updateDescription?.removedFields
+                const ns = change.ns
+                expect(ns).toEqual({ db: config.dbNameTest, coll: config.collectionOnFire })
+                expect(operation).toEqual(changeStreamOperations.update)
+                expect(documentKey._id).toBeDefined()
+                expect(removedFields).toEqual(['str2'])
+                changeStream.close()
+                done()
+            }
+            catch (err) {
+                done(err)
+            }
+        })
+        const batch = async () => {
+            await collection.updateOne(
+                { num1: 1 },
+                { $unset: { str2: '' } }
+            )
+        }
+        setImmediate(batch)
+    })
+
+    test('Update must return some needed fields when truncate array nested field from document', done => {
+        changeStream = collection.watch()
+        changeStream.on('change', change => {
+            try {
+                const operation = change.operationType
+                const documentKey: any = change.documentKey
+                const truncatedArrays = (change.updateDescription as any).truncatedArrays
+                const ns = change.ns
+                expect(ns).toEqual({ db: config.dbNameTest, coll: config.collectionOnFire })
+                expect(operation).toEqual(changeStreamOperations.update)
+                expect(documentKey._id).toBeDefined()
+                expect(truncatedArrays).toEqual([{ field: 'arr', newSize: 3 }])
+                changeStream.close()
+                done()
+            }
+            catch (err) {
+                done(err)
+            }
+        })
+        const batch = async () => {
+            await collection.updateOne(
+                { num1: 1 },
+                [{ $set: { arr: [1, 2, 3] } }]
+            )
+        }
+        setImmediate(batch)
+    })
+})
---
+++ src/services/mongo.test.ts
@@ -0,0 +1,200 @@
+import { ChangeStreamDocument } from 'mongodb'
+import config from '../config'
+import changeStreamOperations from '../enums/changeStreamOperations'
+import environments from '../enums/environments'
+import MongoService from './mongo'
+
+describe('Mongo Service unit and integration tests', () => {
+    let mongoService: MongoService
+
+    beforeAll(() => {
+        mongoService = new MongoService()
+        expect(mongoService).toBeDefined()
+    })
+
+    test('startMongoConnection() has to estabilish connection', async () => {
+        expect(mongoService['collectionData']).not.toBeDefined()
+        expect(mongoService['changeStream']).not.toBeDefined()
+        expect(mongoService['mongoClient']).not.toBeDefined()
+        expect(mongoService['collection']).not.toBeDefined()
+        expect(mongoService['db']).not.toBeDefined()
+        await mongoService.startMongoConnection()
+        expect(mongoService['collectionData']).toBeDefined()
+        expect(mongoService['collectionData'].length).toBeDefined()
+        expect(mongoService['changeStream']).toBeDefined()
+        expect(mongoService['changeStream'].listenerCount('change')).toEqual(1)
+        expect(mongoService['mongoClient']).toBeDefined()
+        expect(mongoService['collection']).toBeDefined()
+        expect(mongoService['collection'].collectionName).toEqual(config.collectionOnFire)
+        expect(mongoService['db']).toBeDefined()
+        expect(mongoService['db'].databaseName).toEqual(config.environment === environments.develop ? config.dbNameDev : config.dbName)
+    })
+
+    test('stopMongoConnection() has to close connection stuff', async () => {
+        await mongoService.stopMongoConnection()
+        expect(mongoService['changeStream'].listenerCount('change')).toEqual(0)
+        expect(mongoService['changeStream'].closed).toBeTruthy()
+    })
+
+    test('Can connect and disconnect again', async () => {
+        await mongoService.startMongoConnection()
+        expect(mongoService['collectionData']).toBeDefined()
+        expect(mongoService['collectionData'].length).toBeDefined()
+        expect(mongoService['changeStream']).toBeDefined()
+        expect(mongoService['changeStream'].listenerCount('change')).toEqual(1)
+        expect(mongoService['mongoClient']).toBeDefined()
+        expect(mongoService['collection']).toBeDefined()
+        expect(mongoService['collection'].collectionName).toEqual(config.collectionOnFire)
+        expect(mongoService['db']).toBeDefined()
+        expect(mongoService['db'].databaseName).toEqual(config.environment === environments.develop ? config.dbNameDev : config.dbName)
+        await mongoService.stopMongoConnection()
+        expect(mongoService['changeStream'].listenerCount('change')).toEqual(0)
+        expect(mongoService['changeStream'].closed).toBeTruthy()
+    })
+
+    test('Chage Stream Listener(change) has to route change events for insert, update, replace and delete operations', async () => {
+        await mongoService.startMongoConnection()
+        try {
+            const insertFunctionMock = jest.spyOn(mongoService, 'insertFromCollection' as never)
+            const updateFunctionMock = jest.spyOn(mongoService, 'updateFromCollection' as never)
+            const replaceFunctionMock = jest.spyOn(mongoService, 'replaceFromCollection' as never)
+            const deleteFunctionMock = jest.spyOn(mongoService, 'deleteFromCollection' as never)
+            mongoService['changeStream'].emit('change', { operationType: changeStreamOperations.insert, documentKey: 'dummyKey' as any } as ChangeStreamDocument)
+            mongoService['changeStream'].emit('change', { operationType: changeStreamOperations.update, documentKey: 'dummyKey' as any } as ChangeStreamDocument)
+            mongoService['changeStream'].emit('change', { operationType: changeStreamOperations.replace, documentKey: 'dummyKey' as any } as ChangeStreamDocument)
+            mongoService['changeStream'].emit('change', { operationType: changeStreamOperations.delete, documentKey: 'dummyKey' as any } as ChangeStreamDocument)
+            expect(insertFunctionMock).toHaveBeenCalledWith({ operationType: changeStreamOperations.insert, documentKey: 'dummyKey' })
+            expect(updateFunctionMock).toHaveBeenCalledWith({ operationType: changeStreamOperations.update, documentKey: 'dummyKey' })
+            expect(replaceFunctionMock).toHaveBeenCalledWith({ operationType: changeStreamOperations.replace, documentKey: 'dummyKey' })
+            expect(deleteFunctionMock).toHaveBeenCalledWith({ operationType: changeStreamOperations.delete, documentKey: 'dummyKey' })
+            jest.resetAllMocks()
+            jest.restoreAllMocks()
+        }
+        finally {
+            await mongoService.stopMongoConnection()
+        }
+    })
+
+    test('private deleteFromCollection() have to remove document right way on collectionData[]', async () => {
+        await mongoService.startMongoConnection()
+        try {
+            mongoService['collectionData'] = [{ _id: 111 }]
+            expect( mongoService['collectionData']).toEqual([{ _id: 111 }])
+            mongoService['deleteFromCollection']({ documentKey: { _id: 111 } as any } as ChangeStreamDocument)
+            expect( mongoService['collectionData']).toEqual([])
+        }
+        finally {
+            await mongoService.stopMongoConnection()
+        }
+    })
+
+    test('private insertFromCollection() have to insert document right way on collectionData[]', async () => {
+        await mongoService.startMongoConnection()
+        try {
+            mongoService['collectionData'] = []
+            expect( mongoService['collectionData']).toEqual([])
+            mongoService['insertFromCollection']({ fullDocument: { _id: 111 } as any } as ChangeStreamDocument)
+            expect( mongoService['collectionData']).toEqual([{ _id: 111 }])
+        }
+        finally {
+            await mongoService.stopMongoConnection()
+        }
+    })
+
+    test('private replaceFromCollection() have to replace document right way on collectionData[]', async () => {
+        await mongoService.startMongoConnection()
+        try {
+            mongoService['collectionData'] = [{ _id: 111, dummy: 'aa' }]
+            expect( mongoService['collectionData']).toEqual([{ _id: 111, dummy: 'aa' }])
+            mongoService['replaceFromCollection']({ 
+                documentKey: { _id: 111 } as any, 
+                fullDocument: { _id: 111, dummy: 'bb' } as any,
+            } as ChangeStreamDocument)
+            expect( mongoService['collectionData']).toEqual([{ _id: 111, dummy: 'bb' }])
+        }
+        finally {
+            await mongoService.stopMongoConnection()
+        }
+    })
+
+    test('private updateFromCollection() have to remove document field right way on collectionData[]', async () => {
+        await mongoService.startMongoConnection()
+        try {
+            mongoService['collectionData'] = [{ _id: 111, dummy: 'aa', dummy2: 'bb' }]
+            expect( mongoService['collectionData']).toEqual([{ _id: 111, dummy: 'aa', dummy2: 'bb' }])
+            mongoService['updateFromCollection']({ 
+                documentKey: { _id: 111 } as any, 
+                updateDescription: { removedFields: ['dummy'] } as any,
+            } as ChangeStreamDocument)
+            expect( mongoService['collectionData']).toEqual([{ _id: 111, dummy2: 'bb' }])
+        }
+        finally {
+            await mongoService.stopMongoConnection()
+        }
+    })
+
+    test('private updateFromCollection() have to truncate document array field right way on collectionData[]', async () => {
+        await mongoService.startMongoConnection()
+        try {
+            mongoService['collectionData'] = [{ _id: 111, arr: [1, 2, 3, 4, 5, 6] }]
+            expect( mongoService['collectionData']).toEqual([{ _id: 111, arr: [1, 2, 3, 4, 5, 6] }])
+            mongoService['updateFromCollection']({ 
+                documentKey: { _id: 111 } as any, 
+                updateDescription: { truncatedArrays: [{ field: 'arr', newSize: 3 }] } as any,
+            } as ChangeStreamDocument)
+            expect( mongoService['collectionData']).toEqual([{ _id: 111, arr: [1, 2, 3] }])
+        }
+        finally {
+            await mongoService.stopMongoConnection()
+        }
+    })
+
+    test('private updateFromCollection() have to update document field right way on collectionData[]', async () => {
+        await mongoService.startMongoConnection()
+        try {
+            mongoService['collectionData'] = [{ _id: 111, dummy: 'aa' }]
+            expect( mongoService['collectionData']).toEqual([{ _id: 111, dummy: 'aa' }])
+            mongoService['updateFromCollection']({ 
+                documentKey: { _id: 111 } as any, 
+                updateDescription: { updatedFields: { dummy: 'bb' } } as any,
+            } as ChangeStreamDocument)
+            expect( mongoService['collectionData']).toEqual([{ _id: 111, dummy: 'bb' }])
+        }
+        finally {
+            await mongoService.stopMongoConnection()
+        }
+    })
+
+    test('private updateFromCollection() have to update document array field right way on collectionData[]', async () => {
+        await mongoService.startMongoConnection()
+        try {
+            mongoService['collectionData'] = [{ _id: 111, arr: [1, 2, 3, 4] }]
+            expect( mongoService['collectionData']).toEqual([{ _id: 111, arr: [1, 2, 3, 4] }])
+            mongoService['updateFromCollection']({ 
+                documentKey: { _id: 111 } as any, 
+                updateDescription: { updatedFields: { 'arr.0': 11, 'arr.1': 22 } } as any,
+            } as ChangeStreamDocument)
+            expect( mongoService['collectionData']).toEqual([{ _id: 111, arr: [11, 22, 3, 4] }])
+        }
+        finally {
+            await mongoService.stopMongoConnection()
+        }
+    })
+
+    test('private updateFromCollection() have to update document array field right way on collectionData[]', async () => {
+        await mongoService.startMongoConnection()
+        try {
+            mongoService['collectionData'] = [{ _id: 111, arr: [1, 2, 3, 4] }]
+            expect( mongoService['collectionData']).toEqual([{ _id: 111, arr: [1, 2, 3, 4] }])
+            mongoService['updateFromCollection']({ 
+                documentKey: { _id: 111 } as any, 
+                updateDescription: { updatedFields: { arr: [11, 22, 33, 44] } } as any,
+            } as ChangeStreamDocument)
+            expect( mongoService['collectionData']).toEqual([{ _id: 111, arr: [11, 22, 33, 44] }])
+        }
+        finally {
+            await mongoService.stopMongoConnection()
+        }
+    })
+
+})
